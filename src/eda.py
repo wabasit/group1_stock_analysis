45993d99777a06c53d656b3af679586f835da170
Hakeem Wikireh <hakeemwikireh@Hakeems-MacBook-Pro.local>
2025-04-03 17:37:58 +0000

    Exploring the structure of the dataset to check for the rows and columns, duplicates and null values

---
+++ src/eda.py
@@ -0,0 +1,18 @@
+
+#Importing Libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_parquet("/Users/hakeemwikireh/Desktop/DataEngineering/GroupAssignment/stockmarket/cleaned-stocks.parquet")
+
+#Data Exploration
+data.head()
+
+data.isnull().sum()
+
+data.duplicated().sum()
+
+data.shape
+print("Total rows: ", data.shape[0])
+print("Total columns: ", data.shape[1])
