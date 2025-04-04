99120a88bb72ceb1ab2c3ab07782808a537ef1b4
awbasit <nanaqwadjobarima@gmail.com>
2025-04-04 02:46:03 +0000

    testing the moving average function

--- test_stock_analysis.py
+++ test_stock_analysis.py
@@ -12,5 +12,10 @@ class TestDataProcessingFunctions(unittest.TestCase):
         self.assertFalse(cleaned_data.isnull().values.any(), 'NaN values exist')
         self.assertEqual(len(cleaned_data), 2, 'Duplicates exist')
 
+    def test_moving_average(self):
+        stock_price = [200,300,400,500]
+        output = [300,400]
+        self.assertEqual(moving_average(stock_price, window=2), output, 'Incorrect Moving average')
+        
 if __name__=='__main__':
     unittest.main()
